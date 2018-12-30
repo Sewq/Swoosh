206a3cf59ae3415fedd8336dfd80de437d91ecb1
Sewq <sewilion@gmail.com>
2018-12-30 18:39:04 +0100

    initial commit

---
+++ app/src/main/java/com/sewq/swoosh/LeagueAvtivity.kt
@@ -0,0 +1,12 @@
+package com.sewq.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueAvtivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league_avtivity)
+    }
+}
