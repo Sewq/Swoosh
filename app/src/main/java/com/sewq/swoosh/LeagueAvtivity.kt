7e4be648ee63c5ff259c1a480566cf06ad956e55
Sewq <sewilion@gmail.com>
2018-12-30 22:54:50 +0100

    added intent

--- app/src/main/java/com/sewq/swoosh/LeagueAvtivity.kt
+++ app/src/main/java/com/sewq/swoosh/LeagueAvtivity.kt
@@ -7,6 +7,6 @@ class LeagueAvtivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league_avtivity)
+        setContentView(R.layout.activity_league)
     }
 }
